7b50163aa84e1f3c52a5d28f346bbb9eff4123c9
evenrv <60918590+evenrv@users.noreply.github.com>
2020-05-18 11:32:11 +0200

    All parsing is now done in 1 seperate class. Created ParseInfo class to reduce global variables and send variables more effectivily.

---
+++ app/src/main/java/com/example/locomotion/Datatype/ParseInfo.java
@@ -0,0 +1,12 @@
+package com.example.locomotion.Datatype;
+
+public class ParseInfo {
+
+    public float mPerLong;
+    public float mPerLat;
+    public float angle;
+    public String room;
+
+    public double[] xcoords;
+    public double[] ycoords;
+}
