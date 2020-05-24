2d6daf6c697e75af58a0919e969fe95e05eb95b8
evenrv <60918590+evenrv@users.noreply.github.com>
2020-05-24 10:48:08 +0200

    dx and dy implemented to obstacle avoidance function

--- app/src/main/java/com/example/locomotion/usb/SerialArduino.java
+++ app/src/main/java/com/example/locomotion/usb/SerialArduino.java
@@ -44,7 +44,7 @@ public class SerialArduino extends AppCompatActivity implements ArduinoListener 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.front_page);
 
         //TODO: lag ny layout for USB_serial
 /*
