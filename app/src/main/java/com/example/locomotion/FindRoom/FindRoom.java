559919319b6828a3d9fc20dc0d2c3a3b9b282f12
evenrv <60918590+evenrv@users.noreply.github.com>
2020-05-25 14:42:33 +0200

    Fixed drive-function

--- app/src/main/java/com/example/locomotion/FindRoom/FindRoom.java
+++ app/src/main/java/com/example/locomotion/FindRoom/FindRoom.java
@@ -11,6 +11,8 @@ public class FindRoom {
 
     public String FindRoomID(String[] RoomID, String[] RoomNumber, String userInput, String building, String floor)
 
+    //As the user may interpret how to enter a room as writing for instance 042 instead of 42, both
+    //ways will be possible, and return 042.
     {
         if (userInput.length() == 2)
         {
@@ -20,13 +22,12 @@ public class FindRoom {
             roomNumber = userInput;
         }
 
-        //Creating the final room number, based on the input from the spinners, and the user input.
+        //Creating the final name, based on the input from the spinners, and the user input.
         room = (building + floor + " " + roomNumber);
 
 
         //Checking if the final romm number is in the array of room numbers. If it is, the
         // corresponding ID will be assigned to TheRoom
-
         for (int i = 0; i < RoomID.length; i++) {
 
             if (RoomNumber[i].contains(room)) {
